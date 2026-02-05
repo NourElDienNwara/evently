d7a965eec5ed2b61a09ea91331167a01a3290643
NourElDienNwara <noureldiennwara312@gmail.com>
2026-02-05 12:17:06 +0200

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:evently/app_theme.dart';
 import 'package:evently/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   return runApp(Evently());
 }
 
